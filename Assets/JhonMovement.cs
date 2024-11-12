e0c43ad7481010bebcb072929e54c5bf16b5c8e4
Richard <ricky201325@gmail.com>
2024-11-12 15:25:57 -0300

    doble salto y suelo

--- Assets/JhonMovement.cs
+++ Assets/JhonMovement.cs
@@ -3,36 +3,50 @@ public class NewBehaviourScript : MonoBehaviour
 {
     private Rigidbody2D Rigidbody2D;
     private float Horizontal;
-    //VARIABLES QUE CONTROLAN LA FUERZA DEL SALTO Y LA VELOCIDAD DEL OBJETO
+    
     public float Speed;
-    public float JumpForce; //VARIABLE PUBLICA PARA AJUSTAR LA FUERZA
+    public float JumpForce;
 
-    //DENTRO DEL PASO 2
     private bool Grounded;
+    private int jumpCount = 0;  // Contador de saltos
+    public int maxJumps = 1;    // Máximo número de saltos permitidos
 
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    //----------------CODIGO QUE DETECTA LOS INPUTS DEL USUARIO----------------
-
     void Update()
     {
         Horizontal = Input.GetAxis("Horizontal");
-        if (Input.GetKeyDown(KeyCode.W)){
+
+        Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red);
+        if(Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
+        {
+            Grounded = true;
+            jumpCount = 0; // Reiniciamos el contador cuando tocamos el suelo
+        }
+        else 
+        {
+            Grounded = false;
+        }
+
+        // Permitimos saltar si aún tenemos saltos disponibles
+        if (Input.GetKeyDown(KeyCode.W) && jumpCount < maxJumps)
+        {
             Jump();
-        };
+            jumpCount++; // Incrementamos el contador de saltos
+        }
     }
 
-    //----------------CODIGO QUE TIENE TODAS LOS RIGIDBODY----------------
     private void Jump()
     {
-    Rigidbody2D.AddForce(Vector2.up * JumpForce); 
-
+        Rigidbody2D.velocity = new Vector2(Rigidbody2D.velocity.x, 0); // Opcional: resetea la velocidad vertical
+        Rigidbody2D.AddForce(Vector2.up * JumpForce); 
     }
+
     private void FixedUpdate()
     {
-        Rigidbody2D.velocity = new Vector2(Horizontal *Speed, Rigidbody2D.velocity.y);
+        Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
     }
 }
